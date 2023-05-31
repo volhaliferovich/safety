5ea0e7d818c7b50bebf085d6a252cb53908e0970
Ольга Олиферович <lelaoliferovic@gmail.com>
2023-05-31 10:45:18 +0300

    adding dist folder

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import './assets/styles/main.css';
-
-import { createApp } from 'vue';
-import App from './App.vue';
-import { i18n } from "@/plugins/i18n";
-
-createApp(App).use(i18n).mount('#app');
